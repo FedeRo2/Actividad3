f6707550c937904dc4a7959187971de889f5c46a
Roxana333 <144742706+Roxana333@users.noreply.github.com>
2023-11-10 22:05:09 -0300

    formulario.

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,4 +1,3 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
   'use strict'
 
@@ -18,43 +17,4 @@
   })
 })()
 
-     function validarDatos( ) { 
 
-    if (formularioconsultas.Nombre.value == 0) {
-        alert('complete el Nombre minimo 3 caracteres');
-        formularioconsultas.Nombre.value = '';
-        formularioconsultas.Nombre.focus();
-        return false;
-
-    }
-    else if (formularioconsultas.Apellido.value == 0) {
-        alert('complete el Nombre minimo 3 caracteres');
-        formularioconsultas.Apellido.value = '';
-        formularioconsultas.Apellido.focus();
-        return false;
-    }
-
-    else if (formularioconsultas.Mail.value == 0) {
-        alert('complete el Apellido minimo 3 caracteres');
-        formularioconsultas.Mail.value = '';
-        formularioconsultas.Mail.focus();
-        return false;
-    }
-    else if (formularioconsultas.Ciudad.value == 0) {
-        alert('complete el Ciudad minimo 3 caracteres');
-        formularioconsultas.Ciudad.value = '';
-        formularioconsultas.Ciudad.focus();
-        return false;
-    }
-
-    else if (formularioconsultas.CONSULTA.value == 0) {
-        alert('complete el CONSULTA minimo 3 caracteres');
-        formularioconsultas.CONSULTA.value = '';
-        formularioconsultas.CONSULTA.focus();
-        return false;
-
-    }
-
-
-
-   }
