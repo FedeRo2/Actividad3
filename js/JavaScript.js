4f5fe21f0808b933be80ec5ac477df01aab23281
Roxana333 <144742706+Roxana333@users.noreply.github.com>
2023-11-16 20:24:29 -0300

    formulario
     interaciones con JavaScrip,
     vínculo con el servicio gratuito para envío de formulario

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,4 +1,5 @@
-(function () {
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
   'use strict'
 
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
@@ -16,5 +17,33 @@
     }, false)
   })
 })()
+
+
+
+function validarDatos() {
+  if(formconsultas.nombre.value ==0) {
+    alert ('completa el nombre minimo 3 caracteres')
+    formconsultas.nombre.value ='';
+    formconsultas.nombre.focus();
+    return false;
+
+  }
+
+   else if(formconsultas.apellido.value ==0) {
+    alert ('completa el apellido minimo 3 caracteres')
+    formconsultas.apellido.value ='';
+    formconsultas.apellido.focus();
+    return false;
+
+  }
+  
+
+  else { alert("Recibimos tu Consulta");
+
+}
+
+
+  
+}
 
 
